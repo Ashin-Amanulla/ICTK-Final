eacbe74bc4349c2ccf9b9988b08be41a8b229e8e
ashin <ashin.jnv@gmail.com>
2021-12-09 23:52:45 +0530

    new

---
+++ FrontEnd/src/app/admin-dashboard/admin-dashboard.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminDashboardRoutingModule } from './admin-dashboard-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminDashboardRoutingModule
+  ]
+})
+export class AdminDashboardModule { }
---
+++ FrontEnd/src/app/landing-page/landing-page.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [
+    CommonModule,
+    LandingPageRoutingModule
+  ]
+})
+export class LandingPageModule { }
