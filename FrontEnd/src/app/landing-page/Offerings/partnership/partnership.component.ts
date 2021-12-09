ef0bff249c071c7b3f31b28aa1951fd2e9606a31
ashin <ashin.jnv@gmail.com>
2021-12-10 00:02:53 +0530

    vh

---
+++ FrontEnd/src/app/landing-page/Offerings/partnership/partnership.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partnership',
+  templateUrl: './partnership.component.html',
+  styleUrls: ['./partnership.component.css']
+})
+export class PartnershipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- FrontEnd/src/app/landing-page/landing-page.module.ts
+++ FrontEnd/src/app/landing-page/landing-page.module.ts
@@ -4,12 +4,28 @@ import { CommonModule } from '@angular/common';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './About/banner/banner.component';
+import { StaffsComponent } from './About/staffs/staffs.component';
+import { TestimonialsComponent } from './Home/testimonials/testimonials.component';
+import { SinglePageComponent } from './Courses/single-page/single-page.component';
+import { MainPageComponent } from './Courses/main-page/main-page.component';
+import { AcademicComponent } from './Offerings/academic/academic.component';
+import { CorporateComponent } from './Offerings/corporate/corporate.component';
+import { PartnershipComponent } from './Offerings/partnership/partnership.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
+    BannerComponent,
+    StaffsComponent,
+    TestimonialsComponent,
+    SinglePageComponent,
+    MainPageComponent,
+    AcademicComponent,
+    CorporateComponent,
+    PartnershipComponent,
   ],
   imports: [
     CommonModule,
