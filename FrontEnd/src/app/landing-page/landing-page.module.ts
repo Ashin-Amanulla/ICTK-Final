ee5fc729aff31da8d0e3034bfda0c5c5cb1784fc
ashin <ashin.jnv@gmail.com>
2021-12-10 20:09:21 +0530

    uig

--- FrontEnd/src/app/landing-page/landing-page.module.ts
+++ FrontEnd/src/app/landing-page/landing-page.module.ts
@@ -12,6 +12,8 @@ import { AcademicComponent } from './Offerings/academic/academic.component';
 import { CorporateComponent } from './Offerings/corporate/corporate.component';
 import { PartnershipComponent } from './Offerings/partnership/partnership.component';
 import { HomeMainComponent } from './Home/home-main/home-main.component';
+import { ContactComponent } from './Home/contact/contact.component';
+import { AboutMainComponent } from './About/about-main/about-main.component';
 
 
 @NgModule({
@@ -26,6 +28,8 @@ import { HomeMainComponent } from './Home/home-main/home-main.component';
     CorporateComponent,
     PartnershipComponent,
     HomeMainComponent,
+    ContactComponent,
+    AboutMainComponent,
   ],
   imports: [
     CommonModule,
