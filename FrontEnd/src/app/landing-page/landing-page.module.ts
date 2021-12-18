74edf2e78665e223f499f0f7fa4ec2eea73e6278
Jeslin Jayan Varghese <jeslinjaisan@gmail.com>
2021-12-18 16:27:14 +0530

    About 2

--- FrontEnd/src/app/landing-page/landing-page.module.ts
+++ FrontEnd/src/app/landing-page/landing-page.module.ts
@@ -11,9 +11,22 @@ import { AcademicComponent } from './Offerings/academic/academic.component';
 import { CorporateComponent } from './Offerings/corporate/corporate.component';
 import { PartnershipComponent } from './Offerings/partnership/partnership.component';
 import { HomeMainComponent } from './Home/home-main/home-main.component';
+
+
+
+import { AboutMainComponent } from './About/about-main/about-main.component';
+import { BannerComponent } from './About/banner/banner.component';
+import { AbtComponent } from './About/abt/abt.component';
+import { FeatureComponent } from './About/feature/feature.component';
+import { LeadersComponent } from './About/leaders/leaders.component';
+import { TeamComponent } from './About/team/team.component';
+import { PatronsComponent } from './About/patrons/patrons.component';
+import { IndustrialPartnersComponent } from './About/industrial-partners/industrial-partners.component';
+import { MarketingPartnersComponent } from './About/marketing-partners/marketing-partners.component';
+import { TrainersComponent } from './About/trainers/trainers.component';
+import { KnowledgePartnersComponent } from './About/knowledge-partners/knowledge-partners.component';
+import { VendorsComponent } from './vendors/vendors.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
-
-
 
 @NgModule({
   declarations: [
@@ -25,7 +38,19 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
     AcademicComponent,
     CorporateComponent,
     PartnershipComponent,
-    HomeMainComponent
+    HomeMainComponent,
+    AboutMainComponent,
+    BannerComponent,
+    AbtComponent,
+    FeatureComponent,
+    LeadersComponent,
+    TeamComponent,
+    PatronsComponent,
+    IndustrialPartnersComponent,
+    MarketingPartnersComponent,
+    TrainersComponent,
+    KnowledgePartnersComponent,
+    VendorsComponent,
   ],
   imports: [
     CommonModule,
