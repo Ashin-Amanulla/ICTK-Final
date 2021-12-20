9d95c8d2cfc4ea8f8867002689612d9dad019202
ashin <ashin.jnv@gmail.com>
2021-12-20 12:43:27 +0530

    20/12

--- FrontEnd/src/app/landing-page/Home/footer/footer.component.ts
+++ FrontEnd/src/app/landing-page/Home/footer/footer.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'app-footer1',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class Footer1Component implements OnInit {
 
   constructor() { }
 
--- FrontEnd/src/app/landing-page/landing-page.module.ts
+++ FrontEnd/src/app/landing-page/landing-page.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+//ashin
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -12,12 +12,15 @@ import { AcademicComponent } from './Offerings/academic/academic.component';
 import { CorporateComponent } from './Offerings/corporate/corporate.component';
 import { PartnershipComponent } from './Offerings/partnership/partnership.component';
 import { HomeMainComponent } from './Home/home-main/home-main.component';
-
+import {AboutMainComponent}  from './About/about-main/about-main.component';
+//sandeep
 import { BannerHomeComponent } from './Home/banner-home/banner-home.component';
 import { VideoComponent } from './Home/video/video.component';
 import { CardsComponent } from './Home/cards/cards.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { TesterComponent } from './Home/tester/tester.component';
+import { Footer1Component } from './Home/footer/footer.component';
+
 
 
 @NgModule({
@@ -32,11 +35,13 @@ import { TesterComponent } from './Home/tester/tester.component';
     CorporateComponent,
     PartnershipComponent,
     HomeMainComponent,
-
+    AboutMainComponent,
+//Sandeep
     BannerHomeComponent,
     VideoComponent,
     CardsComponent,
-    TesterComponent
+    TesterComponent,
+    Footer1Component
     
 
    
